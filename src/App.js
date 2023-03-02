ef922166bba9e7b6756f8915cf89e11f4487e089
mahmoodhamza1261 <mahmoodhamza1261@gmail.com>
2023-03-02 22:59:12 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component, useState } from 'react';
+import './App.css';
+class App extends Component {
+render(){
+	const name='john doe'
+  return (
+    <div className="App">
+     <h1>Hello {name.toUpperCase()}</h1>
+    </div>
+
+   );
+}
+
+
+}
+export default App;
