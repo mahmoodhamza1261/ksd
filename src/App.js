18801e82a6e1f67c116540b677e88fe51aeee26e
mahmoodhamza1261 <mahmoodhamza1261@gmail.com>
2023-03-04 07:07:42 +0500

    4

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Fragment, Component, useState } from 'react';
 import './App.css';
+
+import Navbar from './components/layout/Navbar';
 class App extends Component {
 	render() {
-		const name = 'john doe';
-		const foo = () => 'Bar';
-		const loading = true;
+	
 	
 		return (
 			<div className='App'>
-				{loading?<h4>Hello {name}</h4>:<h4>loading..</h4>}
+				
+        <Navbar />
 			</div>
 		);
 	}
