80db639a3a63d50948ab00182f8085cdcb7c48d8
mahmoodhamza1261 <mahmoodhamza1261@gmail.com>
2023-03-12 20:33:57 +0500

    13

--- src/App.js
+++ src/App.js
@@ -3,45 +3,53 @@ import './App.css';
 import Users from './components/users/Users';
 import Navbar from './components/layout/Navbar';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 import axios from 'axios';
 
 class App extends Component {
-	
-	state={
-		users:[],
+	state = {
+		users: [],
 		loading: false,
-		setAlert:null
-	}
-	
+		setAlert: null,
+	};
+
 	// async componentDidMount(){
-		
+
 	// 	    this.setState({loading: true})
-  //        const res= await axios.get('https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}')
+	//        const res= await axios.get('https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}')
 	// 			 console.log(res.data)
 	// 			 this.setState({users:res.data, loading:false})
 	// }
 	//serach github users
-	searchUsers=async text=>{
-		
-		this.setState({loading: true})
-		const res= await axios.get(`https://api.github.com/search/users?q=${text}&client_id${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
-		console.log(res.data)
-		this.setState({users:res.data.items, loading:false})
-	}
+	searchUsers = async (text) => {
+		this.setState({ loading: true });
+		const res = await axios.get(
+			`https://api.github.com/search/users?q=${text}&client_id${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+		);
+		console.log(res.data);
+		this.setState({ users: res.data.items, loading: false });
+	};
 	//clear users from state
-	clearUsers=()=>this.setState({users:[],loading:false})
+	clearUsers = () => this.setState({ users: [], loading: false });
 
-	setAlert=(msg,type)=>{
-		this.setState({alert:{msg, type}})
-	}
+	setAlert = (msg, type) => {
+		this.setState({ alert: { msg, type } });
+		setTimeout(() => this.setState({ alert: null }), 2000);
+	};
 	render() {
-		const {users,loading}=this.state;
+		const { users, loading } = this.state;
 		return (
 			<div className='App'>
 				<Navbar />
 				<div className='container'>
-					<Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={users.length>0?true:false } setAlert={this.setAlert}/>
-					<Users loading={loading} users={users}/>
+					<Alert alert={this.state.alert} />
+					<Search
+						searchUsers={this.searchUsers}
+						clearUsers={this.clearUsers}
+						showClear={users.length > 0 ? true : false}
+						setAlert={this.setAlert}
+					/>
+					<Users loading={loading} users={users} />
 				</div>
 			</div>
 		);
